cc5c30e1644c45ba5e3d5b61f99b97ee2f2f381a
Guido Menendez <guidomenendez0@gmail.com>
2023-04-22 00:17:44 -0300

    -Se agrega el archivo no subido WholeDayEvent
    PENDIENTE:
    -Refactorizar el metodo nextEventDateTime en la clase FrequencyWeekly.
    -Corregir tests que con los cambios hechos dejaron de funcionar y dividir en paquetes.
    -Corregir relaciones en el UML.
    -Hacer objeto Calendar.
    -Agregar alarmas.

---
+++ src/main/java/org/calendar/event/WholeDayEvent.java
@@ -0,0 +1,18 @@
+package org.calendar.event;
+
+import java.time.LocalDate;
+
+public class WholeDayEvent extends Event {
+    final static int ONE_DAY = 1;
+
+    //Constructor.
+    public WholeDayEvent(int id, String title, String description, LocalDate startDate){
+        super(id, title, description, startDate.atStartOfDay(), startDate.plusDays(ONE_DAY).atStartOfDay());
+    }
+
+    //Post: Sets the startDateTime of the event and consequently the endingDateTime.
+    public void setStartDate(LocalDate startDate){
+        this.startDateTime = startDate.atStartOfDay();
+        this.endingDateTime = startDate.plusDays(ONE_DAY).atStartOfDay();
+    }
+}
