26c784366b448cc4d2e2f4bf97c24fa89fff958f
mauroj4ckunas <96546837+mauroj4ckunas@users.noreply.github.com>
2023-04-30 17:08:39 -0300

    Update: creo un evento con frecuencia infinita

--- src/test/java/org/calendar/event/WholeDayEventTest.java
+++ src/test/java/org/calendar/event/WholeDayEventTest.java
@@ -1,5 +1,7 @@
 package org.calendar.event;
 
+import org.calendar.event.frequency.Frequency;
+import org.calendar.event.frequency.FrequencyDaily;
 import org.junit.Test;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -51,6 +53,29 @@ public class WholeDayEventTest {
 
         assertEquals(startTomorrow, event.getStartDateTime());
         assertEquals(endTomorrow, event.getEndingDateTime());
+    }
+
+    @Test
+    public void eventInfinity(){
+        String title = "Title";
+        String description = "Description";
+        LocalDate now = LocalDate.of(2023,4,30);
+        WholeDayEvent eventInfinity = new WholeDayEvent(title, description, now);
+
+        Frequency frequencyWithoutDeadline = new FrequencyDaily(5);
+
+        eventInfinity.setEventFrequency(frequencyWithoutDeadline);
+
+        LocalDateTime day = eventInfinity.getStartDateTime();
+
+        // Como crear un ciclo infinito es inviable, se simula la infinidad con un ciclo muy grande.
+        // Este evento se repetira un millon de veces.
+        for (int i = 5; i <= 5000000; i += 5){
+            LocalDateTime plusDay = day.plusDays(5);
+            LocalDateTime nextDayEvent = eventInfinity.nextEventDateTime(day);
+            assertEquals(plusDay, nextDayEvent);
+            day = nextDayEvent;
+        }
     }
 
 }
