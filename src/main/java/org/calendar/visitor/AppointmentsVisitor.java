55247713f27d9d4d3e332c9ec17cd3dfd937b232
Guido Menendez <guidomenendez0@gmail.com>
2023-05-06 05:17:16 -0300

    -Se avanza con el metodo GetAppointmentsBetween(); -Correcciones en la documentacion del proyecto (Archivo Calendar.txt).
    PENDIENTE:
    GET_APPOINTMENTS_BETWEEN():
    -Hacer metodos visitPeriodTimeEvent y visitWholeDayEvent.
    -Hacer que los metodos visitantes retornen el array de appointments correspondiente.
    -Hacer que appointment.acceptVisitor() retorne lo que retorna el visitante.
    
    OTROS:
    *Actualizar UML
    *Agregar metodo que recorra todos los appointments pidiendole que updatee sus observadores cada 1 minuto.
    
    -Alarmas:
    *Solucionar problema de que la alarma no puede acceder a la data de su appointment.

--- src/main/java/org/calendar/visitor/AppointmentsVisitor.java
+++ src/main/java/org/calendar/visitor/AppointmentsVisitor.java
@@ -8,13 +8,59 @@ import org.calendar.task.WholeDayTask;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 public class AppointmentsVisitor implements Visitor {
+    //Pre: eventStartTime and firstDateTime must be before eventEndingTime and secondDateTime respectively.
+    //Post: Returns true if startDateTime is between the given dates (or equal firstDateTime) and if endingDateTime is before the given dates (or equal to secondDateTime). False otherwise.
+    private boolean eventIsBetweenDates(LocalDateTime eventStartTime, LocalDateTime eventEndingTime, LocalDateTime firstDateTime, LocalDateTime secondDateTime){
+        return (eventStartTime.isAfter(firstDateTime) || eventStartTime.isEqual(firstDateTime)) && (eventEndingTime.isBefore(secondDateTime) || eventEndingTime.isEqual(secondDateTime));
+    }
+
+    //Pre: eventStartTime and firstDateTime must be before eventEndingTime and secondDateTime respectively.
+    //Post: Returns true if event startDateTime is before firstDateTime and endingDateTime is between the given dates (or equal to any of them). False otherwise.
+    private boolean eventEndsBetweenDates(LocalDateTime eventStartTime, LocalDateTime eventEndingTime, LocalDateTime firstDateTime, LocalDateTime secondDateTime){
+        return (eventStartTime.isBefore(firstDateTime) && (eventEndingTime.isAfter(firstDateTime) || eventEndingTime.isEqual(firstDateTime)) &&
+                (eventEndingTime.isBefore(secondDateTime) || eventEndingTime.isEqual(secondDateTime)));
+    }
+
+    //Pre: eventStartTime and firstDateTime must be before eventEndingTime and secondDateTime respectively.
+    //Post: Returns true if the event starts between dates given (or is equal to any of them) and finishes after. False otherwise.
+    private boolean eventStartsBetweenDates(LocalDateTime eventStartTime, LocalDateTime eventEndingTime, LocalDateTime firstDateTime, LocalDateTime secondDateTime){
+        return (((eventStartTime.isEqual(firstDateTime) || eventStartTime.isAfter(firstDateTime)) && eventStartTime.isBefore(secondDateTime)) && eventEndingTime.isAfter(secondDateTime));
+    }
+
+    //Pre: eventStartTime and firstDateTime must be before eventEndingTime and secondDateTime respectively.
+    //Post: Returns true if the event starts before the firstDatetime and ends after the secondDateTime given.
+    private boolean eventStartsBeforeAndEndsAfterDates(LocalDateTime eventStartTime, LocalDateTime eventEndingTime, LocalDateTime firstDateTime, LocalDateTime secondDateTime){
+        return (eventStartTime.isBefore(firstDateTime) && eventEndingTime.isAfter(secondDateTime));
+    }
+
+    //Pre: eventStartTime and firstDateTime must be before eventEndingTime and secondDateTime respectively.
+    //Post: Returns true if event is taking place between the two dates given. False otherwise.
+    private boolean eventTakesPlacesBetweenDates(LocalDateTime eventStartTime, LocalDateTime eventEndingTime, LocalDateTime firstDateTime, LocalDateTime secondDateTime){
+        return eventIsBetweenDates(eventStartTime, eventEndingTime, firstDateTime, secondDateTime) ||
+               eventEndsBetweenDates(eventStartTime, eventEndingTime, firstDateTime, secondDateTime) ||
+               eventStartsBetweenDates(eventStartTime, eventEndingTime, firstDateTime, secondDateTime) ||
+               eventStartsBeforeAndEndsAfterDates(eventStartTime, eventEndingTime, firstDateTime, secondDateTime);
+    }
+
+    //Pre: Given event must repeat.
+    //Post: Checks if any of the event s repetitions fit between given dates. If so, adds them to selectedAppointments.
+    private void checkRepetitions(PeriodTimeEvent periodTimeEvent, List<Appointment> selectedAppointments, LocalDateTime firstDateTime, LocalDateTime secondDateTime){
+
+    }
+
     //@inheritDoc
     @Override
     public void visitPeriodTimeEvent(PeriodTimeEvent periodTimeEvent, LocalDateTime firstDateTime, LocalDateTime secondDateTime) {
         var selectedAppointments = new ArrayList<Appointment>();
-        System.out.println("Visiting...");
+        var eventStartTime = periodTimeEvent.getStartDateTime();
+        var eventEndingTime = periodTimeEvent.getEndingDateTime();
+
+        if(eventTakesPlacesBetweenDates(eventStartTime, eventEndingTime, firstDateTime, secondDateTime)) selectedAppointments.add(periodTimeEvent);
+
+        if(periodTimeEvent.IsRepeated()) checkRepetitions(periodTimeEvent, selectedAppointments, firstDateTime, secondDateTime);
 
         //return selectedAppointments;
     }
